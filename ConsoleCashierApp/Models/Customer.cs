94980f3f6250147897c7f181693e6627b6c4bbc6
TopherNeil <gomez.cneil@gmail.com>
2024-12-13 01:39:58 +0800

    added a directory for models

---
+++ ConsoleCashierApp/Models/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleCashierApp.Models;
+
+public class Customer
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public bool IsSeniorCitizen { get; set; }
+    public bool IsMember { get; set; }
+    public double TotalPurchased { get; set; }
+    
+}
---
+++ ConsoleCashierApp/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleCashierApp.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public int NumberOfStock { get; set; }
+    
+}
